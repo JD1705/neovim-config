b754513c1c660c966c47871911383eb0b3c2f1be
jd1705 <soulbusterjd17@gmail.com>
2025-12-15 10:32:29 -0400

    create lsp initialization file

---
+++ lsp/init.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'lua',
+  callback = function()
+    -- Cargar configuración solo para archivos Lua
+    require('lsp.lua_ls')
+    vim.lsp.enable("lua_ls")
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'python',
+    once = false, -- Para cada buffer de Python
+    callback = function(args)
+        local bufnr = args.buf
+        -- Pequeño delay para asegurar que todo está listo
+        vim.defer_fn(function()
+            require("lsp.basedpyright")
+            vim.lsp.enable('basedpyright')
+            require("lsp.ruff")
+            vim.lsp.enable('ruff')
+        end, 50)
+    end,
+})
+
+-- require("lsp.basedpyright")
+-- vim.lsp.enable("basedpyright")
